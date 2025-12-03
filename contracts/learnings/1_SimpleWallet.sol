8464ed6f5e62652e808ccd00d8ffec96225d9831
JeffryHermanto <jeffryhermanto@gmail.com>
2025-12-03 08:09:27 +0700

    Initial commit

---
+++ contracts/learnings/1_SimpleWallet.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract SimpleWallet {
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // user bisa menyimpan saldo ke sc (*)
+    // user bisa melihat saldonya sendiri dan saldo user lain
+    // user bisa mentransfer saldo dia ke user lain (*)
+    // user bisa menarik kembali saldonya (*)
+    // (*) = butuh event
+}
